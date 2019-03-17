d9a79572ce4cdf5da9ae50ec695a03cecbed6c5a
Guttz <gustavopguto@gmail.com>
2019-03-17 20:20:31 +0100

    Endpoints added: weather and search

--- main_flask.py
+++ main_flask.py
@@ -1,37 +1,211 @@
 from datetime import datetime
-from flask import Flask, render_template, url_for, flash, redirect
+from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import exc
+from flasgger import Swagger
+from requests import get
+
+import json
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'bookatruckinseconds'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///weather_log.db'
 db = SQLAlchemy(app)
-db.create_all()
+swagger = Swagger(app)
 
 class Weather_Log(db.Model):
-    __tablename__ = 'Weather_Log'
     id = db.Column(db.Integer, primary_key=True)
-    weather = db.Column(db.String(5), nullable=False)
+    weather = db.Column(db.Float, nullable=False)
+    icon = db.Column(db.String(8), nullable=False)
     address = db.Column(db.String(200), unique=True, nullable=False)
     date = db.Column(db.DateTime(), nullable=False, default=datetime.utcnow())
 
     def __repr__(self):
-        return f"User('{self.date}', '{self.address}', '{self.weather}')"
+        return f'{{"date":"{self.date}", "address":"{self.address}", "weather":"{self.weather}"}}'
 
-@app.route("/")
-@app.route("/home")
-def home():
-    new_weatherLog = Weather_Log(address="asddsa", weather="asdasds")
+db.create_all()
+
+API_URL = "http://api.openweathermap.org/data/2.5/weather"
+API_KEY = "b65335b39eb9189980121a51c105c90a"
+
+
+@app.route("/weather", methods=['GET'])
+def get_weather():
+    """Endpoint that returns the weather for given Zip Code and Country Code as input
+    ---
+    parameters:
+      - name: zipCode
+        in: query
+        type: string
+        required: true
+        default: '88000-000'
+      - name: countryCode
+        in: query
+        type: string
+        required: true
+        default: 'br'
+    definitions:
+      Weather:
+        type: object
+        properties:
+          temp:
+            type: number
+          pressure:
+            type: number
+          humidity:
+            type: number
+          temp_min:
+            type: number
+          temp_max:
+            type: number
+      Failed:
+        type: object
+        properties:
+          cod:
+            type: number
+          message:
+            type: string
+    responses:
+      200:
+        description: Successfully retrieved the weather for given Zip Code
+        schema:
+          $ref: '#/definitions/Weather'
+        examples:
+          weather: {"temp": 21.17, "pressure": 1015, "humidity": 83,
+          "temp_min": 15.56, "temp_max": 24}
+      404:
+        description: The specified city wasn't found
+        schema:
+          $ref: '#/definitions/Failed'
+        examples:
+          failed: {"cod":"404","message":"city not found"}
+    """
+    params = {'zip': "88000-000,br", 'APPID': API_KEY, 'units': "metric"}
+
+    if not request.args.get('zipCode') or not request.args.get('countryCode'):
+        return '{"cod":"403", "message":"invalid request"}', 400
+    else:
+        params['zip'] = request.args.get(
+            'zipCode') + "," + request.args.get('countryCode')
+
+    weather_response = get(url=API_URL, params=params)
+
+    if weather_response.json()['cod'] == 200:
+        return json.dumps(weather_response.json()['main']), 200
+    else:
+        return json.dumps(weather_response.json()), 404
+
+
+@app.route("/searches", methods=['GET'])
+def get_searches():
+    """Endpoint that returns the searches already executed
+    ---    
+    definitions:
+      WeatherLogs:
+        type: array
+        items:
+          $ref: '#/definitions/WeatherLog'
+      WeatherLog:
+        type: object
+        properties:
+          date:
+            type: string
+          address:
+            type: string
+          weather:
+            type: number
+          icon:
+            type: string
+    responses:
+      200:
+        description: Successfully retrieved the weather for given Zip Code
+        schema:
+          $ref: '#/definitions/WeatherLogs'
+        examples: [{"date":"2019-03-16 19:50:50.155235", "address":"Florianopólis",
+        "weather":21.23, "icon":"04d.png"}]
+    """
+    return str(Weather_Log.query.all())
+
+
+@app.route("/searches", methods=['POST'])
+def post_searches():
+    """Endpoint that inserts data to the search history
+    ---
+    parameters:
+      - name: WeatherLog
+        description: Object with the searched elements
+        in: body
+        required: true
+        schema:
+            $ref: '#/definitions/WeatherLog'
+    definitions:
+      WeatherLog:
+        type: object
+        required:
+          - address
+          - weather
+          - icon
+        properties:
+          date:
+            type: string
+          address:
+            type: string
+          weather:
+            type: number
+          icon:
+            type: string
+      Failed:
+        type: object
+        properties:
+          cod:
+            type: number
+          message:
+            type: string
+    responses:
+      200:
+        description: Successfully inserted the Weather Log in the Database
+        schema:
+          $ref: '#/definitions/WeatherLog'
+        examples:
+          weather: {"temp": 21.17, "pressure": 1015, "humidity": 83,
+          "temp_min": 15.56, "temp_max": 24}
+      400:
+        description: Invalid request, please check the body of your request
+        schema:
+          $ref: '#/definitions/Failed'
+        examples:
+          failed: {"cod":"400", "message":"invalid request"}
+      404:
+        description: Server internal error when inserting into Database
+        schema:
+          $ref: '#/definitions/Failed'
+        examples:
+          failed: {"cod":"500","message":"internal error"}
+    """
+
+    try:
+        body = request.get_data()
+        body = body.decode('utf8').replace("'", '"')
+        body = json.loads(body)
+    except Exception as exception:
+        print(exception)
+        return '{"cod":"400", "message":"invalid request"}', 400
+
+    if not body['weather'] or not body['icon'] or not body['address']:
+        return '{"cod":"400", "message":"invalid request"}', 400
+
+    new_weatherLog = Weather_Log(weather=body['weather'], icon=body['icon'], address=body['address'])
     db.session.add(new_weatherLog)
     
     try:
         db.session.commit()
-    except exc.IntegrityError as exception:
+        return str(new_weatherLog), 200
+    except Exception as exception:
         print(exception)
-        db.session.rollback() 
+        return '{"cod":"500", "message":"internal server error when commiting to Database"}', 500
 
     return str(Weather_Log.query.all())
+
 
 if __name__ == '__main__':
     app.run(debug=True)
